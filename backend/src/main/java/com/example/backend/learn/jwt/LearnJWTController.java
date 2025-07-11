ed4fd5de7603041776487f07737833fbb0e2dc97
rmrisco28 <rmrisco28@naver.com>
2025-07-11 14:29:10 +0900

    64일차 로그인 권한 연습 완료 jwt, token

---
+++ backend/src/main/java/com/example/backend/learn/jwt/LearnJWTController.java
@@ -0,0 +1,145 @@
+package com.example.backend.learn.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/learn/jwt")
+@RequiredArgsConstructor
+public class LearnJWTController {
+
+    private final JwtEncoder jwtEncoder;
+
+
+    @GetMapping("sub10")
+    @PreAuthorize("hasAuthority('SCOPE_manager')")
+    public String sub10() {
+        System.out.println("LearnJWTController.sub10");
+        return "null";
+    }
+
+
+    @GetMapping("sub9")
+    @PreAuthorize("hasAuthority('SCOPE_admin')")
+    public String sub9Admin() {
+        System.out.println("LearnJWTController.sub9Admin");
+        return "null";
+    }
+
+
+    @GetMapping("sub8")
+    public String sub8() {
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .subject("11@11.com")
+                .issuedAt(Instant.now())
+                .expiresAt(Instant.now().plusSeconds(60 * 60 * 24 * 365))
+//                .claim("scp","admin") // 7
+                .claim("scp", "manager") // 8
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+    @GetMapping("sub7")
+    public String sub7() {
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .subject("11@11.com")
+                .issuedAt(Instant.now())
+                .expiresAt(Instant.now().plusSeconds(60 * 60 * 24 * 365))
+                .claim("scp", "admin") // 7
+//                .claim("scp","manager") // 8
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+    @GetMapping("sub6")
+    public String sub6() {
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .subject("11@11.com")
+                .issuedAt(Instant.now())
+                .expiresAt(Instant.now().plusSeconds(60 * 60 * 24 * 365))
+//                .claim("scp","admin") // 7
+//                .claim("scp","manager") // 8
+                .build();
+
+        return jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+    }
+
+    @GetMapping("sub3")
+    // 유효한 토큰이 있는 요청만 실행 가능 아니면 401 응답
+    // @PreAuthorize 는 configuration 에 @EnableMethodSecurity 있어야 함.
+    @PreAuthorize("isAuthenticated()") // 유효한 토큰이 있는 요청만 실행 가능
+    public String sub3(Authentication authentication) {
+        System.out.println("LearnJWTController.sub3");
+
+        return null;
+    }
+
+
+    // Authentication 객체를 request handler method 파라미터에 두면
+    // jwt가 유효하면 name 속성이 subject(jwt)인 Authentication 객체를 대입
+    //      아니면 null을 대입
+    @GetMapping("sub2")
+    public String sub2(Authentication authentication) {
+        System.out.println("LearnJWTController.sub2");
+        if (authentication == null) {
+            System.out.println("로그인 안했거나, 유효하지 않은 토큰");
+        } else {
+            System.out.println(authentication.getName());
+        }
+        return "";
+    }
+
+    @PostMapping("sub1")
+    public String sub1(@RequestBody Map<String, String> data) {
+        System.out.println("email = " + data.get("email"));
+        System.out.println("password = " + data.get("password"));
+
+        // jwt 토큰 완성
+        // sign 어떻게.정보(claim,payload).sign
+
+        // claim/payload에 민감한 정보 담지 않기(최소한의 정보)
+
+        // 꼭 작성해야 하는 claim들(4개)
+        // 어디서 발행했는지 Issuer (iss)
+        // 누구를 위한 토큰인지 Subject (sub)
+        // 언제 만들었는지 Issued At (iat)
+        // 언제까지 유효한지 Expiration Time (exp)
+
+        // 우리가 필요한 것(1개)
+        // 권한 Scope (scp)
+
+        JwtClaimsSet claimsSet = JwtClaimsSet.builder()
+//                .claim("sub", data.get("email"))
+                .subject(data.get("email"))
+
+//                .claim("iss", "self") 대신에
+                .issuer("self")
+
+//                .claim("iat", Instant.now()) 대신에
+                .issuedAt(Instant.now())
+
+//                .claim("exp", Instant.now().plusSeconds(60 * 60 * 24 * 365)) 대신에
+                .expiresAt(Instant.now().plusSeconds(60 * 60 * 24 * 365))
+
+                .claim("scp", "admin user manager") // space로 구분
+                .build();
+
+        // jwt 응답
+        return jwtEncoder
+                .encode(JwtEncoderParameters.from(claimsSet))
+                .getTokenValue();
+    }
+}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,23 +8,121 @@ import { MemberAdd } from "./feature/member/MemberAdd.jsx";
 import { MemberList } from "./feature/member/MemberList.jsx";
 import { MemberDetail } from "./feature/member/MemberDetail.jsx";
 import { MemberEdit } from "./feature/member/MemberEdit.jsx";
+import axios from "axios";
 
 function App() {
+  function handleButton1Click() {
+    axios
+      .post("/api/learn/jwt/sub1", {
+        email: "son@son.com",
+        password: "son",
+      })
+      .then((res) => {
+        console.log(res.data);
+        // 로컬스토리지에 저장.
+        localStorage.setItem("token", res.data);
+      });
+  }
+
+  // 로그아웃하는 토큰?
+  function handleButton2Click() {
+    localStorage.removeItem("token");
+  }
+
+  function handleButton4Click() {
+    // get, post, put, delete 다 상관없음
+    axios.get("/api/learn/jwt/sub2").then((res) => {});
+  }
+
+  function handleButton3Click() {
+    // localStorage 에서 token 얻기
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      // 토큰 있으면 토큰 들고 요청
+      // Authentization 헤더에 "Bearer " 를 앞에 붙이고
+      axios.get("/api/learn/jwt/sub2", {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+    } else {
+      axios.get("/api/learn/jwt/sub2");
+    }
+  }
+
+  function handleButton5Click() {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      // 토큰 있으면 토큰 들고 요청
+      // Authentization 헤더에 "Bearer " 를 앞에 붙이고
+      axios.get("/api/learn/jwt/sub3", {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+    } else {
+      axios.get("/api/learn/jwt/sub3");
+    }
+  }
+
+  function handleButton6Click() {
+    axios.get("/api/learn/jwt/sub6").then((res) => {
+      const token = res.data;
+      localStorage.setItem("token", token);
+    });
+  }
+
+  function handleButton7Click() {
+    axios.get("/api/learn/jwt/sub7").then((res) => {
+      const token = res.data;
+      localStorage.setItem("token", token);
+    });
+  }
+
+  function handleButton8Click() {
+    axios.get("/api/learn/jwt/sub8").then((res) => {
+      const token = res.data;
+      localStorage.setItem("token", token);
+    });
+  }
+
+  function handleButton10Click() {
+    axios.get("/api/learn/jwt/sub10", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    });
+  }
+
+  function handleButton9Click() {
+    axios.get("/api/learn/jwt/sub9", {
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    });
+  }
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<BoardList />} />
-          <Route path="board/add" element={<BoardAdd />} />
-          <Route path="board/:id" element={<BoardDetail />} />
-          <Route path="board/edit" element={<BoardEdit />} />
-          <Route path="signup" element={<MemberAdd />} />
-          <Route path="member/list" element={<MemberList />} />
-          <Route path="/member" element={<MemberDetail />} />
-          <Route path="/member/edit" element={<MemberEdit />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h3>jwt 로그인 연습</h3>
+      <button onClick={handleButton10Click}>
+        10. manager만 허용 경로로 요청
+      </button>
+      <button onClick={handleButton9Click}>9. Admin만 허용 경로로 요청</button>
+      <button onClick={handleButton8Click}>8. manager 유저 로그인</button>
+      <button onClick={handleButton7Click}>7. admin 유저 로그인</button>
+      <button onClick={handleButton6Click}>6. 일반 유저 로그인</button>
+
+      <button onClick={handleButton5Click}>
+        5. isAuthenticated() 설정된 request handler method에 요청
+      </button>
+      <button onClick={handleButton4Click}>4. 토큰 안들고 요청</button>
+      <button onClick={handleButton3Click}>3. 토큰 들고 요청</button>
+      <button onClick={handleButton2Click}>2. token 지우기(logout)</button>
+      <button onClick={handleButton1Click}>1. token 얻기(login)</button>
+    </div>
   );
 }
 
