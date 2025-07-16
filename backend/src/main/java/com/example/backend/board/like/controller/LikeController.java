1ac7c39cbc004433daead926ae6abac740335b24
rmrisco28 <rmrisco28@naver.com>
2025-07-16 10:36:36 +0900

    67일차 좋아요 업데이트 중 spinner 출력

--- backend/src/main/java/com/example/backend/board/like/controller/LikeController.java
+++ backend/src/main/java/com/example/backend/board/like/controller/LikeController.java
@@ -1,13 +1,11 @@
 package com.example.backend.board.like.controller;
 
+import com.example.backend.board.like.dto.BoardLikeDto;
 import com.example.backend.board.like.dto.LikeForm;
 import com.example.backend.board.like.service.LikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +14,17 @@ public class LikeController {
 
     private final LikeService likeService;
 
+    @GetMapping("board/{boardId}")
+    public BoardLikeDto get(
+            @PathVariable("boardId") Integer boardId,
+            Authentication authentication) {
+
+        return likeService.get(boardId, authentication);
+    }
+
     @PutMapping
     public void like(@RequestBody LikeForm likeForm, Authentication authentication) {
+
         likeService.update(likeForm, authentication);
     }
 }
